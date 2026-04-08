5cfedd9a9267ac82723cdad22599547b2460f891
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-04-09 01:22:31 +0700

    refactor: Rename stopwords variable for consistency and update filtering logic for ease of modification

--- src/config/nlp_config.ts
+++ src/config/nlp_config.ts
@@ -1,11 +1,12 @@
-import { INDONESIAN_STOPWORDS } from "./stopwords";
+import { ID_STOPWORDS } from "./stopwords";
 
-const STOPWORDS_SET = new Set<string>(INDONESIAN_STOPWORDS);
+const STOPWORDS_SET = new Set<string>(ID_STOPWORDS); // Set of words ignored by the NLP
+const IGNORE_LENGTH = 3; // Minimum length of words to be considered by the NLP
 
 export const tokenPreprocessor = (tokens: string[]): string[] => {
   return tokens
     .map((t) => t.trim().toLowerCase())
-    .filter((t) => !STOPWORDS_SET.has(t) && t.length > 2);
+    .filter((t) => !STOPWORDS_SET.has(t) && t.length >= IGNORE_LENGTH);
 };
 
 export const NLP_CONFIG = {
--- src/config/stopwords.ts
+++ src/config/stopwords.ts
@@ -1,4 +1,4 @@
-export const INDONESIAN_STOPWORDS = [
+export const ID_STOPWORDS = [
   "di",
   "ke",
   "dari",
