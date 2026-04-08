6d5953b3fcb60426a899cdc709a3414cced70462
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-04-09 01:49:33 +0700

    feat: Enhance category management with detailed labels and documentation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Citizen Report NLP
 
 To be honest, developing and training an NLP without prior knowledge about AI is the ballsiest thing I've ever done...
+
+## References
+
+- [Peraturan Pemerintah Republik Indonesia Nomor 18 Tahun 2016 Tentang Perangkat Daerah](https://www.bkn.go.id/wp-content/uploads/2016/10/PP-NOMOR-18-TAHUN-2016-PERANGKAT-DAERAH.pdf)
--- src/config/categories.ts
+++ src/config/categories.ts
@@ -1,8 +1,44 @@
+/**
+ * This is where the single source of truth for
+ * categories recognized by the NLP.
+ */
+
+// ! Do not modify
 export const CATEGORIES = {
+  // * Basic Infrastructure
   PUBLIC_WORKS: "PUBLIC_WORKS",
-  SANITATION: "SANITATION",
+  ENVIRONMENT: "ENVIRONMENT",
   TRANSPORTATION: "TRANSPORTATION",
+  WATER_SUPPLY: "WATER_SUPPLY",
+  TELECOMMUNICATION: "TELECOMMUNICATION",
+
+  // * Social Services, Emergency, & Public Order
+  PUBLIC_ORDER: "PUBLIC_ORDER",
+  HEALTH: "HEALTH",
+  FIRE_AND_RESCUE: "FIRE_AND_RESCUE",
+
+  // * System Fallback
   MANUAL_REVIEW: "MANUAL_REVIEW",
 } as const;
 
 export type CategoryName = (typeof CATEGORIES)[keyof typeof CATEGORIES];
+
+// Go ahead and modify if u want :)
+export const CATEGORY_LABELS: Record<CategoryName, string> = {
+  // * Basic Infrastructure
+  [CATEGORIES.PUBLIC_WORKS]: "Dinas Pekerjaan Umum dan Penataan Ruang (DPUPR)",
+  [CATEGORIES.ENVIRONMENT]: "Dinas Lingkungan Hidup dan Kebersihan (DLHK)",
+  [CATEGORIES.TRANSPORTATION]: "Dinas Perhubungan (Dishub)",
+  [CATEGORIES.WATER_SUPPLY]: "Perusahaan Daerah Air Minum (PDAM)",
+  [CATEGORIES.TELECOMMUNICATION]:
+    "Dinas Komunikasi dan Informatika (Diskominfo)",
+
+  // * Social Services, Emergency, & Public Order
+  [CATEGORIES.PUBLIC_ORDER]: "Satuan Polisi Pamong Praja (Satpol PP)",
+  [CATEGORIES.HEALTH]: "Dinas Kesehatan (Dinkes)",
+  [CATEGORIES.FIRE_AND_RESCUE]:
+    "Dinas Pemadam Kebakaran dan Penyelamatan (Damkar)",
+
+  // * System Fallback
+  [CATEGORIES.MANUAL_REVIEW]: "Menunggu Tinjauan Admin",
+};
