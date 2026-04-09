f598038dddcee5ceabc5f2e9dcb23bf1fa5189e9
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-04-09 20:13:58 +0700

    feat: Add training dataset and monitoring script for category counts
    - Created a new JSON file `training_dataset.json` containing various citizen reports categorized by public works, environment, transportation, water supply, telecommunication, public order, health, and fire and rescue.
    - Added a TypeScript script `training_data_monitor.ts` to count and log the number of entries per category from the training dataset.
    - Updated dependencies to include tsx for running TypeScript files

---
+++ test/data/training_data_monitor.ts
@@ -0,0 +1,14 @@
+import data from "../../data/training_dataset.json" with { type: "json" };
+
+// * Count entries per categories
+let counter: Record<string, number> = {};
+data.forEach((entry) => {
+  if (!(entry.category in counter)) {
+    counter[entry.category] = 0;
+  }
+  counter[entry.category]++;
+});
+
+Object.entries(counter).forEach((entry) =>
+  console.log(`${entry[0]}: ${entry[1]}`),
+);
